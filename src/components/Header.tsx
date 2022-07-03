27e115d4beb44e3db04cbf65bafa7ec4d50251f7
Gerardo <gr261212@gmail.com>
2022-07-03 13:55:56 -0400

    better link alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,14 @@ const Header: FC = () => {
   return (
     <header>
       <nav className="max-w-2xl mx-auto pt-8 pb-16">
-        <div className="">
+        <div className="-ml-[0.7rem]">
           <Link href="/">
-            <a className="px-3 py-2 rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
+            <a className="px-3 py-2 relative rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
               Home
             </a>
           </Link>
           <Link href="/about">
-            <a className="px-3 py-2 rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
+            <a className="px-3 py-2 relative rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
               About
             </a>
           </Link>
