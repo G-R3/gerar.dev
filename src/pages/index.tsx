38f565d45163376414fc43985d5f998e2b4233df
Gerardo <gr261212@gmail.com>
2022-07-04 17:12:27 -0400

    display projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import { HiOutlineMail } from "react-icons/hi";
-import { FiGithub } from "react-icons/fi";
+import { FiGithub, FiArrowRight } from "react-icons/fi";
+import ProjectCard from "../components/ProjectCard";
+import { projects } from "../projects";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +11,7 @@ const Home: NextPage = () => {
         <div>
           <h1 className="text-5xl font-bold">Gerardo Rodriguez</h1>
           <h2 className="text-gray-200 text-xl mt-4 mb-6">
-            Web developer and amateur skateboarded
+            Web developer and amateur skateboarder
           </h2>
         </div>
 
@@ -35,6 +37,21 @@ const Home: NextPage = () => {
           >
             <HiOutlineMail size={25} />
           </a>
+        </div>
+      </section>
+      <section className="mt-20">
+        <h3 className="text-4xl font-bold mb-8">Featured Projects</h3>
+        <div className="flex flex-col md:flex-row gap-4">
+          {projects.map((project) => (
+            <ProjectCard key={project.title} {...project} />
+          ))}
+        </div>
+
+        <div className="flex justify-end items-center gap-2 mt-8 text-gray-400 transition-colors group hover:text-white">
+          <a href="https://github.com/G-R3" target="_blank" rel="noreferrer">
+            More of my stuff
+          </a>
+          <FiArrowRight className="group-hover:translate-x-1 transition-transform" />
         </div>
       </section>
     </>
