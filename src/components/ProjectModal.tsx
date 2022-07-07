60a1632b74ae400e378bf5f3ac21b2805e2022d0
Gerardo <gr261212@gmail.com>
2022-07-07 15:55:49 -0400

    updated spacing

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -72,7 +72,7 @@ const ProjectModal: FC<Props> = ({
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 100, transition: { duration: 0.2 } }}
-          className="bg-neutral-900 rounded-md max-w-2xl mx-auto flex flex-col gap-3 p-5"
+          className="bg-neutral-900 rounded-md max-w-lg mx-auto flex flex-col gap-3 p-5"
         >
           {/* <motion.div
             initial={{
@@ -93,9 +93,9 @@ const ProjectModal: FC<Props> = ({
             />
           </motion.div> */}
 
-          <div className="flex flex-col gap-8 mt-2">
+          <div className="flex flex-col mt-2">
             <motion.div initial="hidden" animate="visible" variants={container}>
-              <div className="flex items-center justify-between gap-2 flex-wrap mb-3 md:mb-6">
+              <div className="flex items-center justify-between gap-2 flex-wrap">
                 <Dialog.Title
                   as={motion.h5}
                   variants={fadeIn}
@@ -125,7 +125,7 @@ const ProjectModal: FC<Props> = ({
               <Dialog.Description
                 as={motion.p}
                 variants={fadeIn}
-                className="text-gray-400 text-sm sm:text-base"
+                className="text-gray-400 text-sm sm:text-base mt-3 mb-6"
               >
                 {longDescription}
               </Dialog.Description>
