fdb9b9337e3ce09c70f28d0dbfa97766bdbb29fd
Gerardo <gr261212@gmail.com>
2022-07-06 21:05:04 -0400

    responsiveness

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -32,7 +32,7 @@ const Project: FC<ProjectType> = ({
   externals,
 }) => {
   return (
-    <div className="flex gap-5 relative group">
+    <div className="flex gap-5 flex-col group border-2 border-neutral-800 rounded-md p-2 md:flex-row md:relative md:border-none md:p-0">
       <motion.div
         initial={{ scale: 1.1, opacity: 0 }}
         viewport={{ once: true }}
@@ -41,16 +41,17 @@ const Project: FC<ProjectType> = ({
           scale: 1,
           transition: { duration: 0.5 },
         }}
-        className="w-full md:w-[75%] h-fit bg-cover md:bg-contain bg-no-repeat bg-center grayscale group-hover:filter-none transition-all duration-[400ms]"
+        className="w-full md:w-[75%] h-full"
       >
         <Image
           src={`/images/${image}`}
           alt={title}
           width={2400}
           height={1350}
+          priority
         />
       </motion.div>
-      <div className="flex items-end flex-col px-2 mt-10 absolute right-0 md:mt-20">
+      <div className="flex flex-col px-2 md:items-end md:absolute md:right-0 md:mt-10">
         <motion.h4
           initial={{ opacity: 0, y: 100 }}
           viewport={{ once: true }}
@@ -63,7 +64,7 @@ const Project: FC<ProjectType> = ({
           initial={{ opacity: 0, y: 100 }}
           viewport={{ once: true }}
           whileInView={{ opacity: 1, y: 0 }}
-          className="text-gray-400 my-5 bg-neutral-800 py-3 px-5 rounded-md shadow-lg md:w-[450px] "
+          className="text-gray-400 md:bg-neutral-800 py-3 rounded-md shadow-lg md:w-[450px] md:px-5 md:my-5"
         >
           {longDescription}
         </motion.p>
@@ -72,7 +73,7 @@ const Project: FC<ProjectType> = ({
           viewport={{ once: true }}
           whileInView="visible"
           variants={tagContainer}
-          className="text-sm flex gap-2 text-gray-400 bg-neutral-800 py-1 px-3 rounded-md shadow-lg"
+          className="text-sm flex flex-wrap gap-2 text-gray-400 py-1 rounded-md shadow-lg md:bg-neutral-800 md:px-3"
         >
           {tags.map((tag: string) => (
             <motion.span
