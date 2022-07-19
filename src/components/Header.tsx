b541593da8f4c8ccd6425186f21a4ed28d7de51c
Gerardo <gr261212@gmail.com>
2022-07-19 17:00:03 -0400

    add active class to nav link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,30 @@
 import React, { FC } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NavLink: FC<{ label: string; href: string }> = ({ label, href }) => {
+  const router = useRouter();
+  const isActive = router.pathname === href;
+  return (
+    <Link href={href}>
+      <a
+        className={`text-lg font-normal px-3 py-2 relative rounded-md transition-all ${
+          isActive ? "text-white bg-neutral-900 font-bold" : "text-gray-400"
+        }`}
+      >
+        {label}
+      </a>
+    </Link>
+  );
+};
 
 const Header: FC = () => {
   return (
     <header>
       <nav className="max-w-4xl mx-auto px-5 pt-8 pb-8 md:pb-16">
         <div className="-ml-[0.7rem]">
-          <Link href="/">
-            <a className="text-lg font-normal px-3 py-2 relative rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
-              Home
-            </a>
-          </Link>
-          <Link href="/about">
-            <a className="text-lg font-normal px-3 py-2 relative rounded-md text-gray-400 hover:text-white hover:bg-neutral-900 transition-all">
-              About
-            </a>
-          </Link>
+          <NavLink label={"Home"} href="/" />
+          <NavLink label={"About"} href="/about" />
         </div>
         {/* TODO: theme toggler */}
       </nav>
