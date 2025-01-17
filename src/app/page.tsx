f1d32cb736672696a7e70119bdf447eb1edf7ad5
G-R3 <gr261212@gmail.com>
2025-01-17 03:35:05 -0500

    did stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,101 @@
-import { Github, Linkedin } from "lucide-react";
-import type { NextPage } from "next";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <main className="mt-16">
-      <div className="space-y-4">
-        <p className="text-fg-muted">
-          Hey there, I&apos;m{" "}
-          <span className="font-medium text-fg">Gerardo</span> and welcome to my
-          little corner on the internet. I&apos;m on a journey to{" "}
-          <span className="line-through">break</span>{" "}
-          <span className="font-medium text-fg">build</span> cool things on the
-          web and <span className="font-medium text-fg">learn</span> as much as
-          I can.
-        </p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-        <p className="text-fg-muted">
-          Outside that I like{" "}
-          <span className="font-medium text-fg">skateboarding</span>, playing{" "}
-          <span className="font-medium text-fg">video games</span>,{" "}
-          <span className="font-medium text-fg">jamming out</span> out to some
-          soul and classic rock, and{" "}
-          <span className="font-medium text-fg">
-            obessing over pixels on my screen
-          </span>
-          ,
-        </p>
-      </div>
-
-      <div className="mt-12 flex flex-col gap-5">
-        <span>Find me on</span>
-
-        <div className="flex gap-6 text-fg-muted">
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            href="https://github.com/G-R3"
-            className="flex items-center gap-1 underline underline-offset-4 transition-colors duration-200 hover:text-fg focus-visible:text-fg"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
-            rel="noreferrer noopener"
+            rel="noopener noreferrer"
           >
-            <Github className="mr-1 size-4" />
-            Github
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
           </a>
           <a
-            href="www.linkedin.com/in/gerardo-rodriguez-823551213"
-            className="flex items-center gap-1 underline underline-offset-4 transition-colors duration-200 hover:text-fg focus-visible:text-fg"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
-            rel="noreferrer noopener"
+            rel="noopener noreferrer"
           >
-            <Linkedin className="mr-1 size-4" />
-            LinkedIn
+            Read our docs
           </a>
         </div>
-      </div>
-    </main>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
   );
-};
-
-export default Home;
+}
